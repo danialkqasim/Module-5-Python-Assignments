6dc5d9e770f5efd7518a70002e95ad7653d1bdd6
danialkqasim <dqasim@ivytech.edu>
2026-04-20 21:33:43 -0400

    Final Submission: Completed URLs, Views, HTML, and ORM modules

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    # Fetch posts that have a publication date in the past
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
